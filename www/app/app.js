4de5f5a4c03f729b4445c04d14a8686e95bcfe27
Ivan Rodrigues <ivanknow@gmail.com>
2016-02-11 12:43:25 -0300

    side menu

--- www/app/app.js
+++ www/app/app.js
@@ -2,29 +2,30 @@
  * 
  */
 
-angular.module("eliteApp",["ionic"])
-.run(function($ionicPlatform) {
-	  $ionicPlatform.ready(function() {
+angular.module("eliteApp", [ "ionic" ]).run(function($ionicPlatform) {
+	$ionicPlatform.ready(function() {
 
-	    if (window.cordova && window.cordova.plugins.Keyboard) {
-	      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-	      cordova.plugins.Keyboard.disableScroll(true);
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			cordova.plugins.Keyboard.disableScroll(true);
 
-	    }
-	    if (window.StatusBar) {
-	      // org.apache.cordova.statusbar required
-	      StatusBar.styleDefault();
-	    }
-	  });
-	}).config(function($stateProvider, $urlRouterProvider) {
-		  $stateProvider
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+}).config(function($stateProvider, $urlRouterProvider) {
+	$stateProvider
 
-		    .state('home', {
-		    url: '/home',
-		    templateUrl: 'app/home/home.html'
-		  });
-		  
-		  
-		  // if none of the above states are matched, use this as the fallback
-		  $urlRouterProvider.otherwise('/home');
-		});
+	.state('home', {
+		url : '/home',
+		templateUrl : 'app/home/home.html'
+	}).state('app', {
+		url : '/app',
+		templateUrl : 'app/layout/menu-layout.html'
+	});
+
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/app');
+});
