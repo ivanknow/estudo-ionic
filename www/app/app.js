ace8e3f8c3af60f719296fa52e92fbe0643a015b
Ivan Rodrigues <ivanknow@gmail.com>
2016-02-10 21:13:33 -0300

    tabs

---
+++ www/app/app.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+angular.module("eliteApp",["ionic"])
+.run(function($ionicPlatform) {
+	  $ionicPlatform.ready(function() {
+
+	    if (window.cordova && window.cordova.plugins.Keyboard) {
+	      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	      cordova.plugins.Keyboard.disableScroll(true);
+
+	    }
+	    if (window.StatusBar) {
+	      // org.apache.cordova.statusbar required
+	      StatusBar.styleDefault();
+	    }
+	  });
+	}).config(function($stateProvider, $urlRouterProvider) {
+		  $stateProvider
+
+		    .state('home', {
+		    url: '/home',
+		    templateUrl: 'app/home/home.html'
+		  });
+		  
+		  
+		  // if none of the above states are matched, use this as the fallback
+		  $urlRouterProvider.otherwise('/home');
+		});
