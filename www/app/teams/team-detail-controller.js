9c21a5da1df64cfc966dc9a4556c28b48653f571
Ivan Rodrigues <ivanknow@gmail.com>
2016-02-12 11:32:45 -0300

    grid system

--- www/app/teams/team-detail-controller.js
+++ www/app/teams/team-detail-controller.js
@@ -1,14 +1,31 @@
 /**
  * 
- */
+
 
 (function(){
 	
 	'use strict';
-	angular.module('eliteApp').controller("TeamDetailCtrl",['$stateParams',TeamDetailCtrl])
 	
-	function TeamDetailCtrl($stateParams){
+	angular.module('eliteApp').controller("TeamDetailCtrl",['eliteApi','$stateParams',TeamDetailCtrl])
+	
+	function TeamDetailCtrl($stateParams,eliteApi){
 		var vm = this;
-		console.log("$stateParams",$stateParams);
+		//var data =  {teams:[{"name":"fezes"}]};
+		var data = eliteApi.getLeagueData();
+		vm.team = data.teams[0];
 	}
+})(); */
+
+(function() {
+	'use strict';
+
+	angular.module('eliteApp').controller('TeamDetailCtrl',
+			['eliteApi', TeamDetailCtrl ]);
+
+	function TeamDetailCtrl(eliteApi) {
+		var that = this;
+		var data =  eliteApi.getLeagueData();
+		that.team = data.teams[0].divisionTeams[0].name;
+	};
+
 })();
